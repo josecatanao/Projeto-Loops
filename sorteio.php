10ebe817e016e32c99d315c0b57c597265d8ffe2
josecatanao <51245690+josecatanao@users.noreply.github.com>
2020-07-10 12:28:01 -0300

    addpaginadeerrodosorteio

--- sorteio.php
+++ sorteio.php
@@ -2,25 +2,28 @@
 
 require 'arquivos/config/classes.php';
 $participantes=$banco->mostraParticipantes();
-shuffle($participantes);
-$array1 = $participantes;
-shuffle($participantes);
-$array2 = $participantes;
-$pronto=0;
-while($pronto!=count($array1)){
-	$pronto=0;
-	for ($i=0; $i<count($array1); $i++){
-		if ($array1[$i]['nome']!=$array2[$i]['nome']) {
-		//echo($array1[$i]['nome']."--".$array2[$i]['nome']."<br>");
-			$pronto++;
-		}else{
-			shuffle($array1);
-			shuffle($array2);
-		}
-	} 
+
+if(count($participantes)>2){
+    shuffle($participantes);
+        $array1 = $participantes;
+        shuffle($participantes);
+        $array2 = $participantes;
+        $pronto=0;
+        while($pronto!=count($array1)){
+            $pronto=0;
+            for ($i=0; $i<count($array1); $i++){
+                if ($array1[$i]['nome']!=$array2[$i]['nome']) {
+                //echo($array1[$i]['nome']."--".$array2[$i]['nome']."<br>");
+                    $pronto++;
+                }else{
+                    shuffle($array1);
+                    shuffle($array2);
+                }
+            } 
+        }
+}else{
+     header("Location:arquivos/alertas/alertasorteio.php");
 }
-
-
 
 ?>
 
